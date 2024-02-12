36914cb9f0c743399cad195c021c585661e9be5f
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2024-02-12 21:08:09 +0100

    code style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,18 +19,26 @@ export default function Navbar(): ReactElement {
     return (
         <div className="navbar bg-neutral text-neutral-content">
             <div className="flex-1">
-                <Link className="btn btn-ghost text-xl" href="/">Nils Hindermann</Link>
+                <Link className="btn btn-ghost text-xl" href="/">
+                    Nils Hindermann
+                </Link>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><NavbarLink href="/about">Über mich</NavbarLink></li>
+                    <li>
+                        <NavbarLink href="/about">Über mich</NavbarLink>
+                    </li>
                 </ul>
             </div>
         </div>
     );
 }
 
-function NavbarLink({href, children}: any): ReactElement {
+function NavbarLink({ href, children }: any): ReactElement {
     const pathname = usePathname();
-    return <Link href={href} className={pathname == href ? "font-bold" : ""}>{children}</Link>
+    return (
+        <Link href={href} className={pathname == href ? 'font-bold' : ''}>
+            {children}
+        </Link>
+    );
 }
