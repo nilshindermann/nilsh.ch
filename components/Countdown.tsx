c38e451b23d654edbda96b5089ff1f5c85dd4ff0
Nils Hindermann <nilshindermann@gmail.com>
2024-02-13 15:50:10 +0100

    fix countdown

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactElement, useEffect, useState } from 'react';
+import { CSSProperties, ReactElement, useEffect, useState } from 'react';
 
 export default function Countdown(): ReactElement {
     const [time, setTime] = useState({
@@ -41,25 +41,33 @@ export default function Countdown(): ReactElement {
         <div className="grid auto-cols-max grid-flow-col gap-5 text-center">
             <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
                 <span className="countdown-days countdown font-mono text-5xl">
-                    <span style={{ '--value': time.days }}></span>
+                    <span
+                        style={{ '--value': time.days } as CSSProperties}
+                    ></span>
                 </span>
                 Tage
             </div>
             <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
                 <span className="countdown font-mono text-5xl">
-                    <span style={{ '--value': time.hours }}></span>
+                    <span
+                        style={{ '--value': time.hours } as CSSProperties}
+                    ></span>
                 </span>
                 Std
             </div>
             <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
                 <span className="countdown font-mono text-5xl">
-                    <span style={{ '--value': time.minutes }}></span>
+                    <span
+                        style={{ '--value': time.minutes } as CSSProperties}
+                    ></span>
                 </span>
                 Min
             </div>
             <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
                 <span className="countdown font-mono text-5xl">
-                    <span style={{ '--value': time.seconds }}></span>
+                    <span
+                        style={{ '--value': time.seconds } as CSSProperties}
+                    ></span>
                 </span>
                 Sek
             </div>
