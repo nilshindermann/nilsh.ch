6042023c86060146acca7a39a8ea7241dcdee871
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2024-03-02 20:38:40 +0100

    support years

--- components/countdown/Countdown.tsx
+++ components/countdown/Countdown.tsx
@@ -1,9 +1,16 @@
 'use client';
 
 import EventDate, { getDateFromEvent } from '@/models/EventDate';
-import { CSSProperties, ReactElement, useEffect, useState } from 'react';
+import {
+    CSSProperties,
+    ReactElement,
+    useCallback,
+    useEffect,
+    useState,
+} from 'react';
 
 interface Time {
+    years: number;
     days: number;
     hours: number;
     minutes: number;
@@ -15,11 +22,14 @@ export default function Countdown({
 }: {
     event: EventDate;
 }): ReactElement {
-    const calcTime = (): Time => {
+    const calcTime = useCallback((): Time => {
         const date = getDateFromEvent(event).getTime();
         const now = new Date().getTime();
         const distance = date - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+        const days = Math.floor(
+            (distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24),
+        );
         const hours = Math.floor(
             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
         );
@@ -27,12 +37,13 @@ export default function Countdown({
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         return {
+            years: Math.abs(years),
             days: Math.abs(days),
             hours: Math.abs(hours),
             minutes: Math.abs(minutes),
             seconds: Math.abs(seconds),
         };
-    };
+    }, [event]);
 
     const [time, setTime] = useState<Time>(calcTime);
 
@@ -46,6 +57,16 @@ export default function Countdown({
 
     return (
         <div className="grid auto-cols-max grid-flow-col gap-2 text-center sm:gap-3 lg:gap-4">
+            {time.years > 0 && (
+                <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
+                    <span className="countdown font-mono text-5xl">
+                        <span
+                            style={{ '--value': time.years } as CSSProperties}
+                        ></span>
+                    </span>
+                    Jahre
+                </div>
+            )}
             <div className="flex flex-col rounded-box bg-neutral p-2 text-neutral-content">
                 <span className="countdown-days countdown font-mono text-5xl">
                     <span
