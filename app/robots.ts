5ec8c8707b1c2e29311d4b3ba82a74d8a0597b42
Nils Hindermann <nilshindermann@gmail.com>
2024-04-25 16:23:58 +0200

    update robots.txt sitemap.xml files (#5)
    use CF_PAGES_URL if BASE_URL is not defined

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,10 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl: string = process.env.BASE_URL ?? 'https://www.nilsh.ch';
+    const baseUrl: string =
+        process.env.BASE_URL ??
+        process.env.CF_PAGES_URL ??
+        'https://www.nilsh.ch';
 
     return {
         rules: {
