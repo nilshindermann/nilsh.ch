f9c62b3c275e34e51b197e14180becbe11adaa3f
Nils Hindermann <nilshindermann@gmail.com>
2024-04-26 09:42:09 +0200

    Revert "restructure project and add special page (#8)"
    This reverts commit 6911099bdf18358021f2d47cd1e9ef0ef3129039.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React, { ReactElement } from 'react';
@@ -19,7 +21,15 @@ export default function RootLayout({
 }): ReactElement {
     return (
         <html lang="de-ch">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <div className="flex min-h-screen flex-col">
+                    <Navbar />
+                    <main className="flex flex-1 flex-col items-center justify-center p-8 text-center">
+                        {children}
+                    </main>
+                    <Footer />
+                </div>
+            </body>
         </html>
     );
 }
