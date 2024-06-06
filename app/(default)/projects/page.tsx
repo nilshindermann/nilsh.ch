ada297fc98c0e7d398dd01d0c0115690f5fee374
Nils Hindermann <nilshindermann@gmail.com>
2024-06-06 16:26:18 +0200

    Update /projects page

--- app/(default)/projects/page.tsx
+++ app/(default)/projects/page.tsx
@@ -11,8 +11,28 @@ export default function ProjectsPage(): ReactElement {
         <>
             <h1 className="mb-5 text-4xl font-bold text-primary">Projekte</h1>
             <p>
-                Hier werden in Zukunft die Projekte von Nils Hindermann
-                aufgeführt.
+                Hier findest du eine Übersicht aller Projekte, an denen ich so
+                mitgewirkt habe.
+            </p>
+            <h2>Aktuell Projekte</h2>
+            <p>
+                Aktuell gibt es keine Projekte, die ich hier aufführen möchte.
+            </p>
+            <h2>Vergangene Projekte</h2>
+            <p>
+                Die meisten meiner Projekte sind privat und nicht öffentlich,
+                jedoch gibt es einige Projekte, die ich auf GitHub
+                veröffentlicht habe.
+            </p>
+            <p>
+                Hier eine Auswahl:
+                <ul>
+                    <li>
+                        <a href="https://github.com/nilshindermann">
+                            Projekt 1
+                        </a>
+                    </li>
+                </ul>
             </p>
         </>
     );
