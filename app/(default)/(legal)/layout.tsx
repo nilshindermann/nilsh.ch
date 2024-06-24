b2bde79d5e37aa2ab06da25ea73ff20bf91da098
Nils Hindermann <nilshindermann@gmail.com>
2024-06-24 10:50:59 +0200

    use css module for styling

--- app/(default)/(legal)/layout.tsx
+++ app/(default)/(legal)/layout.tsx
@@ -1,11 +1,11 @@
-import './legal.css';
-
 import { ReactElement } from 'react';
+
+import s from './legal.module.css';
 
 export default function LegalLayout({
     children,
 }: {
     children: ReactElement;
 }): ReactElement {
-    return <div className="max-w-3xl p-4 text-left">{children}</div>;
+    return <div className={s.legal}>{children}</div>;
 }
