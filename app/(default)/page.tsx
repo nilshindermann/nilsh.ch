686a0f6c93ec91bf48cb3b6c7e49f34e9174e4fd
Nils Hindermann <nilshindermann@gmail.com>
2024-07-01 14:59:34 +0200

    prettier

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -7,7 +7,7 @@ export default function Home(): ReactElement {
             <h1 className="font-sans text-4xl font-bold text-primary md:text-6xl">
                 Nils Hindermann
             </h1>
-            <p className="mt-8 text-xl ">
+            <p className="mt-8 text-xl">
                 Hi, ich bin Nils Hindermann 😊
                 <br />
                 Meine Hobbys sind Orientierungslauf & Geocaching.
