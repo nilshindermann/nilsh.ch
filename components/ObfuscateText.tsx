6057ff107b891f099678cc8ec4b011bf16fe7238
Nils Hindermann <nilshindermann@gmail.com>
2024-10-07 11:05:59 +0200

    update ObfuscateText.tsx

--- components/ObfuscateText.tsx
+++ components/ObfuscateText.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import { Obfuscate } from '@south-paw/react-obfuscate-ts';
+import { Obfuscate, ObfuscateProps } from '@south-paw/react-obfuscate-ts';
 import React, { ReactElement } from 'react';
-
-interface ObfuscateTextProps {
-    children: string;
-}
 
 export default function ObfuscateText({
     children,
     ...props
-}: ObfuscateTextProps): ReactElement {
+}: ObfuscateProps): ReactElement {
     return (
         <Obfuscate as={'span'} {...props}>
             {children}
