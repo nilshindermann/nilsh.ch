99a0f5e39d3fc8db566bcc89507bc18d74eb224b
Nils Hindermann <nilshindermann@gmail.com>
2024-11-06 15:37:58 +0100

    move data to separate folder

--- app/(default)/orienteering/page.tsx
+++ app/(default)/orienteering/page.tsx
@@ -1,68 +1,7 @@
+import { seasons } from '@/data/orienteering_seasons';
 import { Metadata } from 'next';
 import Link from 'next/link';
-import React, { ReactElement } from 'react';
-
-interface Season {
-    year: number;
-    category: string;
-    additionalCategories?: Array<string>;
-}
-
-const seasons: Array<Season> = [
-    {
-        year: 2012,
-        category: 'H10',
-    },
-    {
-        year: 2013,
-        category: 'H10',
-    },
-    {
-        year: 2014,
-        category: 'H10',
-    },
-    {
-        year: 2015,
-        category: 'H12',
-    },
-    {
-        year: 2016,
-        category: 'H12',
-    },
-    {
-        year: 2017,
-        category: 'H14',
-    },
-    {
-        year: 2018,
-        category: 'H14',
-    },
-    {
-        year: 2019,
-        category: 'H16',
-    },
-    {
-        year: 2020,
-        category: 'H16',
-    },
-    {
-        year: 2021,
-        category: 'H18',
-    },
-    {
-        year: 2022,
-        category: 'H18',
-    },
-    {
-        year: 2023,
-        category: 'HAK',
-    },
-    {
-        year: 2024,
-        category: 'HAK',
-        additionalCategories: ['HAM'],
-    },
-];
+import { ReactElement } from 'react';
 
 export const metadata: Metadata = {
     title: 'OL - Nils Hindermann',
