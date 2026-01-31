5626dac8f62c52fba7bbd146377815c927c46536
Nils Hindermann <nilshindermann@gmail.com>
2026-01-31 20:25:36 +0100

    Update /orienteering page (#25)

--- app/(default)/orienteering/page.tsx
+++ app/(default)/orienteering/page.tsx
@@ -1,34 +1,108 @@
+import { seasons } from '@/data/orienteering_seasons';
 import { Metadata } from 'next';
 import Link from 'next/link';
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 
 export const metadata: Metadata = {
     title: 'OL - Nils Hindermann',
     description:
-        'Orientierungslauf (OL) ist eines meiner Hobbys seit 2013 und ich bin in der OLG Stäfa tätig.',
+        'Orientierungslauf (OL) ist eines meiner Hobbys seit 2012 und ich bin in der OLG Stäfa tätig.',
 };
 
 export default function OrienteeringPage(): ReactElement {
+    const currentSeason = seasons.find(
+        (season) => season.year === new Date().getFullYear(),
+    );
+
+    const buildLink = (category: string, year: number): string => {
+        return `https://www.o-l.ch/cgi-bin/pliste?cat=${category}&result=list&year=${year}#:~:text=Nils%20Hindermann`;
+    };
+
     return (
         <>
             <h1 className="text-primary mb-5 text-4xl font-bold">
                 Orientierungslauf
             </h1>
             <p>
-                OL ist eines meiner Hobbys. Seit 2013 bin ich Mitglied der OLG
-                Stäfa.
+                OL ist eines meiner Hobbys. Ich bin Mitglied bei der OLG Stäfa.
             </p>
-            <p>
-                Aktuell laufe ich in der Kategorie <b>HAK</b> mit dem Ziel, ein
-                gutes Resultat in der{' '}
-                <Link
-                    href="https://www.o-l.ch/cgi-bin/pliste?cat=HAK&result=list"
-                    target="_blank"
-                >
-                    Jahrespunkteliste
-                </Link>{' '}
-                zu erreichen.
-            </p>
+            {(currentSeason && (
+                <p>
+                    Aktuell laufe ich in der Kategorie{' '}
+                    <b>{currentSeason.category}</b> mit dem Ziel, ein gutes
+                    Resultat in der{' '}
+                    <Link
+                        href={buildLink(
+                            currentSeason.category,
+                            currentSeason.year,
+                        )}
+                        target="_blank"
+                    >
+                        Jahrespunkteliste
+                    </Link>{' '}
+                    zu erreichen.
+                    {currentSeason.additionalCategories && (
+                        <>
+                            {' '}
+                            {currentSeason.additionalCategories.length === 1
+                                ? 'Zusätzlich laufe ich in der Kategorie '
+                                : 'Zusätzlich laufe ich in den Kategorien '}
+                            {currentSeason.additionalCategories.join(', ')}.
+                        </>
+                    )}
+                </p>
+            )) || <p>Ich habe keine aktive Saison.</p>}
+            <h2 className="text-primary mt-5 text-2xl font-bold">
+                Vergangene Saisons
+            </h2>
+
+            <div className="max-w-full overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Jahr</th>
+                            <th>Kategorie(n)</th>
+                            <th>Links</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[...seasons]
+                            .reverse()
+                            .filter((season) => season !== currentSeason)
+                            .map((season) => (
+                                <tr key={season.year}>
+                                    <td>
+                                        <span className="text-primary">
+                                            {season.year}
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <b>{season.category}</b>
+                                        {season.additionalCategories && (
+                                            <>
+                                                ,{' '}
+                                                {season.additionalCategories.join(
+                                                    ', ',
+                                                )}
+                                            </>
+                                        )}
+                                    </td>
+                                    <td>
+                                        <Link
+                                            href={buildLink(
+                                                season.category,
+                                                season.year,
+                                            )}
+                                            target="_blank"
+                                        >
+                                            Jahrespunkteliste
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
---
+++ data/orienteering_seasons.ts
@@ -0,0 +1,69 @@
+export interface Season {
+    year: number;
+    category: string;
+    additionalCategories?: Array<string>;
+}
+
+export const seasons: Array<Season> = [
+    {
+        year: 2012,
+        category: 'H10',
+    },
+    {
+        year: 2013,
+        category: 'H10',
+    },
+    {
+        year: 2014,
+        category: 'H10',
+    },
+    {
+        year: 2015,
+        category: 'H12',
+    },
+    {
+        year: 2016,
+        category: 'H12',
+    },
+    {
+        year: 2017,
+        category: 'H14',
+    },
+    {
+        year: 2018,
+        category: 'H14',
+    },
+    {
+        year: 2019,
+        category: 'H16',
+    },
+    {
+        year: 2020,
+        category: 'H16',
+    },
+    {
+        year: 2021,
+        category: 'H18',
+    },
+    {
+        year: 2022,
+        category: 'H18',
+    },
+    {
+        year: 2023,
+        category: 'HAK',
+    },
+    {
+        year: 2024,
+        category: 'HAK',
+        additionalCategories: ['HAM'],
+    },
+    {
+        year: 2025,
+        category: 'HAM',
+    },
+    {
+        year: 2026,
+        category: 'HAK',
+    },
+];
