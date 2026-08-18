d6594bec944925210f9ee53bbeff794378fd8b6b
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2026-08-18 23:23:20 +0200

    Download coming soon

--- app/(default)/games/catch-me-if-you-can/downloads/page.tsx
+++ app/(default)/games/catch-me-if-you-can/downloads/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import React, { ReactElement } from 'react';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
     title: 'Catch Me If You Can - Nils Hindermann',
@@ -13,6 +14,13 @@ export default function AppleVsApplesPage(): ReactElement {
     return (
         <>
             <h2>Downloads</h2>
+            <Image
+                width={308}
+                height={380}
+                src="/assets/games/catch_me_if_you_can_logo.webp"
+                alt="Catch Me If You Can"
+            />
+            <p>Download noch nicht verfügbar.</p>
         </>
     );
 }
